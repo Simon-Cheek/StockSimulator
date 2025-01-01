3eabc5daea1908c36d3145ec00357077fa8876ee
Simon-Cheek <simon.cheek@yahoo.com>
2025-01-01 15:33:21 -0700

    Debug logs: middleware

--- stock-simulator/src/middleware.ts
+++ stock-simulator/src/middleware.ts
@@ -2,12 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import { authenticateUser } from "./functions/authenticate";
 
 export async function middleware(req: NextRequest) {
+  console.log("Middleware running");
   const apiKey = req.cookies.get("stockSimKey")?.value || "";
   const userID = req.cookies.get("stockSimUser")?.value || "";
   const { pathname } = req.nextUrl;
 
   // Allow login page and static assets
   if (pathname.startsWith("/login") || pathname.startsWith("/_next")) {
+    console.log("Starts with login or _next");
     return NextResponse.next();
   }
   // Authenticate User
@@ -16,11 +18,13 @@ export async function middleware(req: NextRequest) {
       throw Error("Missing auth information");
     }
     const auth = await authenticateUser(userID, apiKey);
+    console.log("AUTH: ", auth);
     if (!auth) {
       throw Error("Unauthorized");
     }
-  } catch {
+  } catch (e) {
     // Redirect
+    console.log("Error: ", e);
     const loginUrl = req.nextUrl.clone();
     loginUrl.pathname = "/login";
     return NextResponse.redirect(loginUrl);
